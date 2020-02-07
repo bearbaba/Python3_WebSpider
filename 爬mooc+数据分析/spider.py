7fd3017d76e8b140bc29b80f12af7377c7d462b8
bearbaba <2200522850@qq.com>
2020-02-07 20:55:21 +0800

    爬取mooc数据科学类课程的相关信息

--- 爬mooc+数据分析/spider.py
+++ 爬mooc+数据分析/spider.py
@@ -1,35 +1,46 @@
 class Spider():
     def __init__(self):
-        self.el_name=[]
-        self.el_lang=[]
-        self.el_level=[]
-        self.el_heat=[]
-    def get_content(self): 
+        self.el_name_sum = []
+        self.el_lang_sum = []
+        self.el_level_sum = []
+        self.el_heat_sum = []
+
+    def get_responses(self,url):
         import requests
+        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0 Win64x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'''}
+        response = requests.get(url, headers=headers)
+        print(response.status_code)
+        #html=open("mooc"+url[-1]+".html","w",encoding="utf-8")
+        #html.write(response.text)
+        #html.close()
+        self.get_content(response.text)
+
+    def get_content(self,response):
         import bs4
         import re
-        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0 Win64x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'''}
-        response = requests.get("https://www.mooc.cn/data-science", headers=headers)
-        print(response.status_code)
-        html=open("mooc.html","w",encoding="utf-8")
-        html.write(response.text)
-        html.close()
-        bsobj=bs4.BeautifulSoup(response.text)
+        bsobj=bs4.BeautifulSoup(response)
         el_name = bsobj.select("h1.courselist-title a")
-
         for i in el_name:
-            self.el_name.append(i.get_text())
+            self.el_name_sum.append(i.get_text())
         el_lang = bsobj.select(".courselang")
         for i in el_lang:
-            self.el_lang.append(i.get_text())
+            self.el_lang_sum.append(i.get_text())
         el_level = bsobj.select("div.courselist-meta>span+span")
         for i in el_level:
             pattern=re.compile(".{1,2}（.{2}）")
             if re.match(pattern,i.get_text())!=None:
-                self.el_level.append(i.get_text())
+                self.el_level_sum.append(i.get_text())
             pattern=re.compile(".{2} [0-9]{1,},?[0-9]{1,}")
             if re.match(pattern,i.get_text())!=None:
-                self.el_heat.append(i.get_text())
+                self.el_heat_sum.append(i.get_text())
+
+    def get_urls(self):
+        urls = []
+        for i in range(1, 8):
+            url = "https://www.mooc.cn/data-science/page/{}".format(i)
+            urls.append(url)
+        return urls
+
     def save(self):
         import xlwt
         workbook=xlwt.Workbook()
@@ -38,12 +49,13 @@ class Spider():
         sheet.write(0,1,'课程语言')
         sheet.write(0,2,'课程难度')
         sheet.write(0,3,'课程热度')
-        for i in range(1,len(self.el_name)+1):
-            sheet.write(i,0,self.el_name[i-1])
-            sheet.write(i,1,self.el_lang[i-1])
-            sheet.write(i,2,self.el_level[i-1])
-            sheet.write(i,3,self.el_heat[i-1])
-        workbook.save(r'D:\computer_language\Python_Study\Python_爬虫\爬mooc\mooc.xlsx')
+        for i in range(1,len(self.el_name_sum)+1):
+            sheet.write(i,0,self.el_name_sum[i-1])
+            sheet.write(i,1,self.el_lang_sum[i-1])
+            sheet.write(i,2,self.el_level_sum[i-1])
+            sheet.write(i,3,self.el_heat_sum[i-1])
+        workbook.save(r'D:\computer_language\Python_Study\Python_爬虫\爬mooc+数据分析\mooc.xlsx')
+
     def show(self):
         import pandas as pd
         pd.read_excel('mooc.xlsx')
@@ -51,8 +63,12 @@ class Spider():
         
 
 if __name__ == "__main__":
+    import multiprocessing
+    pool=multiprocessing.Pool(multiprocessing.cpu_count())
     s=Spider()
-    s.get_content()
+    pool.map(s.get_responses,s.get_urls())
+    pool.close()
+    pool.join()
     s.save()
-    s.show()
+    #s.show()
     
